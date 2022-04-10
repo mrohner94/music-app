ff0f27f4b9ee2871c265d53983cabbdb6f7e8482
Mike <mrohner320@gmail.com>
2022-04-10 18:51:08 -0400

    update signin/signup and authorization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,9 +34,13 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      {Component.authPage ? (
+        <Component {...pageProps} />
+      ) : (
       <PlayerLayout>
         <Component {...pageProps} />
       </PlayerLayout>
+      )}
     </ChakraProvider>
   )
 }
