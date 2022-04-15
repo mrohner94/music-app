942d657a3610f3a5a8cd20e418ce780008188a30
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2022-04-14 21:33:36 -0400

    gradient layout ui update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,14 @@ import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <GradientLayout color="red" roundImage>
+    <GradientLayout 
+      color="red" 
+      subtitle="profile" 
+      title="Mike" 
+      description="15 public playlists" 
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2" 
+      roundImage
+    >
       <div>Home Page</div>
     </GradientLayout>
   )
