14f73dc06af783e7a2b1ef81d42f8e4b8b7a07e4
Mike <mrohner320@gmail.com>
2022-04-25 16:03:51 -0400

    Dynamic UI, reset db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,42 @@
-import GradientLayout from '../components/gradientLayout'
-import prisma from '../lib/prisma'
-import { Box, Flex, Text } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/react'
+import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
 const Home = ({ artists }) => {
+  const { user, isLoading } = useMe();
+
+  if (isLoading) return null; //todo: better loading ui
+
   return (
-    <GradientLayout 
-      color="blue" 
-      subtitle="profile" 
-      title="Mike" 
-      description="15 public playlists" 
-      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2" 
+    <GradientLayout
+      color="blue"
+      subtitle="profile"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
       roundImage
     >
       <Box color="white" paddingX="40px">
         <Box marginBottom="40px">
-          <Text fontSize="2xl" fontWeight="bold">Top artists this month</Text>
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
           <Text fontSize="md">only visible to you</Text>
         </Box>
         <Flex>
           {artists.map((artist) => (
-            <Box paddingX="10px" width="20%">
+            <Box key={artist.artistId} paddingX="10px" width="20%">
               <Box bg="gray.900" borderRadius="4px" width="100%" padding="15px">
-                <Image src="https://placekitten.com/g/300/300" borderRadius="100%"/>
+                <Image
+                  src="https://placekitten.com/g/300/300"
+                  borderRadius="100%"
+                />
                 <Box marginTop="20px">
-                  <Text fontSize="md" fontWeight="bold">{artist.name}</Text>
+                  <Text fontSize="md" fontWeight="bold">
+                    {artist.name}
+                  </Text>
                   <Text fontSize="x-small">Artist</Text>
                 </Box>
               </Box>
@@ -33,15 +45,15 @@ const Home = ({ artists }) => {
         </Flex>
       </Box>
     </GradientLayout>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
-  const artists = await prisma.artist.findMany({})
+  const artists = await prisma.artist.findMany({});
 
   return {
     props: { artists },
-  }
-}
+  };
+};
 
 export default Home;
